34ac91828e4beaff2bbe21e32e13acc464462252
Alyssa Cox <cox24018@byui.edu>
2026-01-15 15:11:04 -0700

    my r function

---
+++ R/multiplydivide.r
@@ -0,0 +1,10 @@
+#' multiplies 2 numbers and divides by a third
+#' @description This function takes in 3 numbers, multiplies the first two and divides by the third
+#' @param m1 is the first number in the multiplication
+#' @param m2 is the second number in the multiplication
+#' @param d1 is the divisor in the function
+#' @example multiplydivide(4, 5, 2)
+#' @export 
+multiplydivide <- function(m1, m2, d1) {
+  m1 * m2 / d1
+}
