44b63117b156bbb28cf3f799ac9fcd957f5efab6
PhilipRosenquist <ros21033@byui.edu>
2026-01-15 15:11:08 -0700

    Add3 function

---
+++ R/add3.r
@@ -0,0 +1,10 @@
+#' An example 3 parts addition function
+#' @description This function takes three numbers and combines them using addition
+#' @param n1 the first number to be added
+#' @param n2 the second number to be added
+#' @param n3 the third number to be added
+#' @example add3(5, 6, 7)
+#' @export
+add3 <- function(n1, n2, n3){
+  n1 + n2 + n3
+}
